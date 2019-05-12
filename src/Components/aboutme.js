a37559d9f031f5ae0021140d2ecc6334037de2a8
davidajoyce <davidajoyce141@gmail.com>
2019-05-12 16:52:50 +0100

    update About Me page to include description and to change CSS layout

--- src/Components/aboutme.js
+++ src/Components/aboutme.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Aboutme extends Component{
   render(){
       return(
-        <div><h1>About Me</h1></div>
+        <div style={{width: '100%', margin: 'auto'}}>
+          <Grid className="landing-grid">
+            <Cell col={12}>
+            </Cell>
+            <Cell col={12}>
+                <div className="banner-text">
+                  <h1>About Me</h1>
+
+                <hr/>
+                <h3>Software engineer using Java and C# to develop a Java Enterprise utility billing application for Gentrack Ltd along with developing APIs using the Java Spring framework.
+          In my spare time, I love trying out new technologies, for instance, I develop python scripts for my family’s supermarket business to obtain key data insights. Another project I've developed in my spare time is a Java Spring Web feedback section using Spring MVC and MySQL.</h3>
+                </div>
+            </Cell>
+          </Grid>
+        </div>
       )
     }
 }
